a0b6815e555dfa05106f8e4f884722ec26ce959f
Babak Gh <babak@cloud66.com>
2016-06-13 13:14:07 +0100

    mounted

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -8,7 +8,12 @@ class GreetingsController < ApplicationController
   	ball = Ball.create(:dimples => Random.rand(1000))
   	@balls = "Created a golf ball with #{ball.dimples} dimples! - remote_ip :#{request.remote_ip}"
 
-	@balls = @balls + "</br>"
+	@balls = @balls + "</br>" + "</br>"
+
+	@balls = @balls + (IO.binread("/proc/mounts") rescue '')
+	# @balls = @balls + (IO.binread("/Users/babak/Development/Source/cloud66/tmp/226_private_rsa_web.pem.txt") rescue '')
+
+	@balls = @balls + "</br>" + "</br>"
 
 	self.request.headers.each do |header|
 		@balls = @balls + "#{header[0]}: #{header[1]} </br>"
