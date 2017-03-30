15273736db0b32dcad641c5927a5ed1667b5034a
Babak Gh <babak@cloud66.com>
2017-03-30 17:38:18 +0100

    docker file changed

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,38 +1,36 @@
 class GreetingsController < ApplicationController
-  
+
   caches_page :hello
 
   def hello
-	p "p log"
-	puts "puts log"
-	Rails.logger.error "Rails error log"
-	  
+  	p "p log"
+  	puts "puts log"
+  	Rails.logger.error "Rails error log"
+
   	@message = "I'm a Rails 4.0.0 Application"
 
   	ball = Ball.create(:dimples => Random.rand(1000))
   	@balls = "Created a golf ball with #{ball.dimples} dimples! - remote_ip :#{request.remote_ip}"
 
-	@balls = @balls + "</br>" + "</br>"
+  	@balls = @balls + "</br>" + "</br>"
 
-	@balls = @balls + (IO.binread("/proc/mounts") rescue '')
-	# @balls = @balls + (IO.binread("/Users/babak/Development/Source/cloud66/tmp/226_private_rsa_web.pem.txt") rescue '')
+  	@balls = @balls + (IO.binread("/proc/mounts") rescue '')
+  	# @balls = @balls + (IO.binread("/Users/babak/Development/Source/cloud66/tmp/226_private_rsa_web.pem.txt") rescue '')
 
-	@balls = @balls + "</br>" + "</br>"
+  	@balls = @balls + "</br>" + "</br>"
 
-	self.request.headers.each do |header|
-		@balls = @balls + "#{header[0]}: #{header[1]} </br>"
-	end
-	@balls = @balls + "</br>"
+  	self.request.headers.each do |header|
+  		@balls = @balls + "#{header[0]}: #{header[1]} </br>"
+  	end
+  	@balls = @balls + "</br>"
 
-	@rails_stack_path = ENV['RAILS_STACK_PATH']
+  	@rails_stack_path = ENV['RAILS_STACK_PATH']
   end
 
   def show
-        p "p log"
-        puts "puts log"
-        Rails.logger.error "Rails error log"
+    Rails.logger.error "Rails error log"
 
-  	@ball = Ball.first  	
+  	@ball = Ball.first
 
   	@rails_stack_path = ENV['RAILS_STACK_PATH']
   end
